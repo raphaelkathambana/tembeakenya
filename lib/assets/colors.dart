0e8bc08da99a8705a2436d85fa5bd822ac512fb9
Raphael Kathambana <maya12raph@gmail.com>
2024-05-20 15:16:44 +0300

    refactor: Add color constants for light and dark themes

---
+++ lib/assets/colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ColorsUtil {
+  static const Color primaryColorLight = Color(0xff84873B);
+  static const Color secondaryColorLight = Color(0xFFcfb796);
+  static const Color accentColorLight = Color(0xFFc4df81);
+  static const Color textColorLight = Color(0xff070901);
+  static const Color backgroundColorLight = Color(0xffecf0e5);
+
+  static const Color primaryColorDark = Color(0xffc1c478);
+  static const Color secondaryColorDark = Color(0xFF695130);
+  static const Color accentColorDark = Color(0xFF637e20);
+  static const Color textColorDark = Color(0xfffcfef6);
+  static const Color backgroundColorDark = Color(0xff161a0f);
+}
+//  $themes: (
+//  light: (
+//    text: #070901,
+//    background: #ecf0e5,
+//    primary: #84873b,
+//    secondary: #cfb796,
+//    accent: #c4df81,
+//  ),
+//  dark: (
+//    text: #fcfef6,
+//    background: #161a0f,
+//    primary: #c1c478,
+//    secondary: #695130,
+//    accent: #637e20,
+//  ),
+// );
