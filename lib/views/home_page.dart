56fde7f1d51e06a8789140f11637c6e7df0a3f64
BethTesHS <131249570+BethTesHS@users.noreply.github.com>
2024-06-23 12:50:50 +0300

    Tembea Kenya with Firebase

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,26 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:tembeakenya/constants/routes.dart';
-import 'package:tembeakenya/controllers/auth_controller.dart';
-import 'package:tembeakenya/main.dart';
-import '../../assets/colors.dart';
+import 'package:tembeakenya/assets/colors.dart';
 
-class HomeView extends StatefulWidget {
+class HomeView extends StatelessWidget {
   const HomeView({super.key});
-
-  @override
-  State<HomeView> createState() => _HomeViewState();
-}
-
-class _HomeViewState extends State<HomeView> {
-  bool _isLoading = false;
-  late NavigationService navigationService;
-
-  @override
-  void initState() {
-    navigationService = NavigationService(router);
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,57 +12,21 @@ class _HomeViewState extends State<HomeView> {
         automaticallyImplyLeading: false,
         backgroundColor: ColorsUtil.backgroundColorDark,
         title: const Text('Home Page',
-            style: TextStyle(
-              color: ColorsUtil.primaryColorLight,
-              fontSize: 35,
-              fontWeight: FontWeight.bold,
-            )),
+            style: TextStyle(color: ColorsUtil.textColorDark)),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(children: [
-            const Text('Welcome'),
-            Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: IconButton(
-                  icon: _isLoading
-                      ? const CircularProgressIndicator(color: Colors.white)
-                      : const Icon(Icons.logout),
-                  onPressed: _isLoading ? null : _handleLogout,
-                )),
-            ElevatedButton(
-                onPressed: () => context.go('/test-view'),
-                style: const MainPage().raisedButtonStyle,
-                child: const Text('ViewTest')),
-          ]),
-        ),
+      body: Column(children: [
+        const Text('Welcome'),
+       
+        TextButton(
+          onPressed: () async {
+            await FirebaseAuth.instance.signOut();
+            if (!context.mounted) return;
+            Navigator.of(context)
+              .pushNamedAndRemoveUntil('/welcome/', (route) => false);
+          },
+          child: const Text("Logout"))
+        ]
       ),
     );
-  }
-
-  Future<void> _handleLogout() async {
-    setState(() {
-      _isLoading = true;
-    });
-
-    bool isLoggedOut = await AuthController(navigationService).logout();
-
-    setState(() {
-      _isLoading = false;
-    });
-
-    if (!mounted) return;
-    if (isLoggedOut) {
-      // Navigate to the login or welcome screen
-      if (!mounted) return;
-      // context.go('/login');
-      navigationService.navigateToLogin(context);
-    } else {
-      // Show an error message
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Logout failed')),
-      );
-    }
   }
 }
