af3c209d76da07056f4240e47d34d1dbe04edaea
Raphael Kathambana <maya12raph@gmail.com>
2024-07-01 15:24:02 +0300

    added mapping packages, and flutter dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:tembeakenya/assets/colors.dart';
 import 'package:tembeakenya/constants/routes.dart';
@@ -10,6 +11,7 @@ import 'package:tembeakenya/views/welcome_view.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp();
   runApp(
     MaterialApp.router(
