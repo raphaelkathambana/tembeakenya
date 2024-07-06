cf4f7d0ec79207138b2f783815539ba6c5470a67
Raphael Kathambana <maya12raph@gmail.com>
2024-07-06 03:13:54 +0300

    chore: Add additional user properties to User model

--- lib/model/user.g.dart
+++ lib/model/user.g.dart
@@ -13,10 +13,15 @@ User _$UserFromJson(Map<String, dynamic> json) => User(
       username: json['username'] as String?,
       email: json['email'] as String?,
       image_id: json['image_id'] as String?,
-      roleNo: json['roleNo'] as String?,
+      roleNo: (json['roleNo'] as num?)?.toInt(),
       email_verified_at: json['email_verified_at'] == null
           ? null
           : DateTime.parse(json['email_verified_at'] as String),
+      no_of_hikes: (json['no_of_hikes'] as num?)?.toInt(),
+      total_distance_walked: (json['total_distance_walked'] as num?)?.toInt(),
+      no_of_steps_taken: (json['no_of_steps_taken'] as num?)?.toInt(),
+      followers_count: (json['followers_count'] as num?)?.toInt(),
+      following_count: (json['following_count'] as num?)?.toInt(),
     );
 
 Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
@@ -28,4 +33,9 @@ Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'image_id': instance.image_id,
       'roleNo': instance.roleNo,
       'email_verified_at': instance.email_verified_at?.toIso8601String(),
+      'no_of_steps_taken': instance.no_of_steps_taken,
+      'total_distance_walked': instance.total_distance_walked,
+      'no_of_hikes': instance.no_of_hikes,
+      'followers_count': instance.followers_count,
+      'following_count': instance.following_count,
     };
